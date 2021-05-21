92f6504acb719befad2faad7f9201cc6eef4311e
toilahuong <toilahuong01@gmail.com>
2021-05-21 12:32:27 +0700

    update

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 8888;
+
+
 const apiRouter = require('./routers/api.route');
 app.use('/api',apiRouter);
+
 app.listen(PORT, (req,res) => {
     console.log(`SERVER CONNECTED WITH PORT: ${PORT}`);
 })
